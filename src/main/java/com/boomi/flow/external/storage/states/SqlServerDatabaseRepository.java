471c98438b1d163bb00e22faf0bd4f358bb4f957
Jose Collazzi <josecollazzi@gmail.com>
2018-07-13 14:59:05 +0100

    CORE-4367 fixing sql server state upsert query

--- src/main/java/com/boomi/flow/external/storage/states/SqlServerDatabaseRepository.java
+++ src/main/java/com/boomi/flow/external/storage/states/SqlServerDatabaseRepository.java
@@ -29,10 +29,10 @@ public class SqlServerDatabaseRepository extends StateDatabaseRepository {
     @Override
     protected String upsertQuery() {
         return "MERGE states as Target " +
-                "USING states as states2 ON states2.id = :id " +
-                "WHEN MATCHED AND states2.updated_at <= :updatedAt THEN " +
-                "  UPDATE  SET flow_id =:flow, flow_version_id =:flowVersion, is_done =:isDone, current_map_element_id=:currentMapElement, current_user_id=:currentUser, updated_at =:updatedAt, content=:content " +
+                "USING states as Source ON Target.id = Source.id AND Source.id = :id " +
+                "WHEN MATCHED AND Target.updated_at <= :updatedAt THEN " +
+                    "UPDATE  SET flow_id =:flow, flow_version_id =:flowVersion, is_done =:isDone, current_map_element_id=:currentMapElement, current_user_id=:currentUser, updated_at =:updatedAt, content=:content " +
                 "WHEN NOT MATCHED THEN " +
-                "INSERT (id, tenant_id, parent_id, flow_id, flow_version_id, is_done, current_map_element_id, current_user_id, created_at, updated_at, content) VALUES (:id, :tenant, :parent, :flow, :flowVersion, :isDone, :currentMapElement, :currentUser, :createdAt, :updatedAt, :content);";
+                    "INSERT (id, tenant_id, parent_id, flow_id, flow_version_id, is_done, current_map_element_id, current_user_id, created_at, updated_at, content) VALUES (:id, :tenant, :parent, :flow, :flowVersion, :isDone, :currentMapElement, :currentUser, :createdAt, :updatedAt, :content);";
     }
 }
