c7bdfb997adba0a9b61946ba5c291f3340188b98
Jonjo McKay <jonjo.mckay@manywho.com>
2018-07-13 12:47:18 +0100

    CORE-4367: Added support for Heroku PostgreSQL

--- src/main/java/com/boomi/flow/external/storage/guice/JdbiProvider.java
+++ src/main/java/com/boomi/flow/external/storage/guice/JdbiProvider.java
@@ -10,9 +10,16 @@ public class JdbiProvider implements Provider<Jdbi> {
     @Override
     public Jdbi get() {
         HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setPassword(Environment.get("DATABASE_PASSWORD"));
-        hikariConfig.setUsername(Environment.get("DATABASE_USERNAME"));
-        hikariConfig.setJdbcUrl(Environment.get("DATABASE_URL"));
+
+        // We do this to support Heroku out of the box
+        var jdbcDatabaseUrl = System.getenv("JDBC_DATABASE_URL");
+        if (jdbcDatabaseUrl != null) {
+            hikariConfig.setJdbcUrl(System.getenv("JDBC_DATABASE_URL"));
+        } else {
+            hikariConfig.setPassword(Environment.get("DATABASE_PASSWORD"));
+            hikariConfig.setUsername(Environment.get("DATABASE_USERNAME"));
+            hikariConfig.setJdbcUrl(Environment.get("DATABASE_URL"));
+        }
 
         return Jdbi.create(new HikariDataSource(hikariConfig));
     }
